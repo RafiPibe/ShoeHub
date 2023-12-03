f536557bb6a42201b65e194b43ea5e696e01d9be
Pascalrjt <pascal.rogerjt@gmail.com>
2023-12-03 18:04:04 +0700

    added delete item from cart

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -5,42 +5,27 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap">
-            @forelse($cartItems as $item)
-            <a href="{{ route('shoe.details', $item->shoe->id) }}">
-                <div class="p-6 text-gray-900 bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-col w-1/5">
-                    <img src="data:image/png;base64, {{ $item->shoe->shoeImage }}" alt="shoe Image" class="max-w-full">
-                    <a> {{ $item->shoe->shoeName }}</a>
-                    <a>Size: {{ $item->shoe->shoeSize }}</a>
-                </div>
-            </a>
-
-            <div class="p-6 text-gray-900">
-                {{ $item->shoe->shoeDescription }}
-            </div>
-            @empty
-                <p>Your cart is empty</p>
-            @endforelse
-        </div>
-    </div> --}}
-
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex">
         <div class="w-3/4">
-            @forelse($cartItems as $item)
-            <a href="{{ route('shoe.details', $item->shoe->id) }}">
-                <div class="p-6 text-gray-900 bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-row w-1/4">
-                    <img src="data:image/png;base64, {{ $item->shoe->shoeImage }}" alt="shoe Image" class="max-w-full">
-                    <div>
-                        <a> {{ $item->shoe->shoeName }}</a>
-                        <a>Size: {{ $item->shoe->shoeSize }}</a>
+                @forelse($cartItems as $item)
+                <a href="{{ route('shoe.details', $item->shoe->id) }}">
+                    <div class="p-6 text-gray-900 bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-row w-full h-screen/4 my-4 relative" style="height: 20vh;">
+                        <img src="data:image/png;base64, {{ $item->shoe->shoeImage }}" alt="shoe Image" class="max-w-full h-full mx-4">
+                        <div class="flex flex-col mx-4">
+                            <div>
+                                <a> {{ $item->shoe->shoeName }}</a><br>
+                                <a>Size: {{ $item->shoe->shoeSize }}</a>
+                            </div>
+                            <form action="{{ route('cart.remove', $item->id) }}" method="POST" class="absolute bottom-4 right-4">
+                                @csrf
+                                @method('DELETE')
+                                <x-secondary-button type="submit">
+                                    {{ __('Remove item from cart') }}
+                                </x-secondary-button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </a>
-
-            {{-- <div class="p-6 text-gray-900">
-                {{ $item->shoe->shoeDescription }}
-            </div> --}}
+                </a>
             @empty
                 <p>Your cart is empty</p>
             @endforelse
