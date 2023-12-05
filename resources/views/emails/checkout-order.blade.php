63bd88d5fe8fec7cc4d46b0ec791cc9dd498a735
Pascalrjt <pascal.rogerjt@gmail.com>
2023-12-05 18:16:17 +0700

    fixed order email

--- resources/views/emails/checkout-order.blade.php
+++ resources/views/emails/checkout-order.blade.php
@@ -1,10 +1,11 @@
 <h1>Your Order Confirmation</h1>
 
-<p>Thank you for your purchase. Here are the details of your order:</p>
+<p>Thank you for your purchase at ShoeHub. We are currently getting your order ready</p>
+<p>Here are the details of your order:</p>
 
 <ul>
     @foreach($cartItems as $item)
-        <li>{{ $item->shoeName }} - ${{ $item->shoePrice }}</li>
+        <li>{{ $item->shoe->shoeName }} - ${{ $item->shoe->shoePrice }}</li>
     @endforeach
 </ul>
 
